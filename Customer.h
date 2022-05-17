05304a35dda5b6fc90fffb10500e85cac1c671d9
RafaelBicario <rafabicario@outlook.com>
2022-05-17 20:57:54 -0300

    Estrutura menu Customer

--- Customer.h
+++ Customer.h
@@ -2,8 +2,10 @@
 #define _market_customer_h_
 
 #include <string>
+#include "Util.h"
 
 using namespace std;
+using namespace Util;
 
 class Customer {
 private:
@@ -14,6 +16,36 @@ public:
     explicit Customer() = default;
 
     string getCustomerName();
+
+    void CustomerMenu()
+    {
+        switch (menuCustomer()) {
+            case 1:
+                showCustomer();
+                enterToContinue()
+                break;
+
+            case 2:
+                registerCustomer();
+                enterToContinue();
+                break;
+
+            case 3:
+                updateCustomer();
+                enterToContinue();
+                break;
+
+            case 4:
+                deleteCustomer();
+                enterToContinue();
+                break;
+
+            default:
+                break;
+        }
+    }
 };
+
+
 
 #endif
