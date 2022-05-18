d7dc598388bd39a8ad4d0921a4580258474fc6eb
RafaelBicario <rafabicario@outlook.com>
2022-05-17 21:00:19 -0300

    Estrutura menu Product

--- Product.h
+++ Product.h
@@ -1,6 +1,10 @@
 #ifndef _market_product_h_
 #define _market_product_h_
 
+#include "Util.h"
+
+using namespace Util;
+
 class Product {
 private:
     int id{};
@@ -11,6 +15,34 @@ public:
     explicit Product() = default;
 
     char* getProductName();
+
+    void ProductMenu()
+    {
+        switch (menuProduct()) {
+            case 1:
+                showProduct();
+                enterToContinue()
+                break;
+
+            case 2:
+                registerProduct();
+                enterToContinue();
+                break;
+
+            case 3:
+                updateProduct();
+                enterToContinue();
+                break;
+
+            case 4:
+                deleteProduct();
+                enterToContinue();
+                break;
+
+            default:
+                break;
+        }
+    }
 };
 
 #endif
