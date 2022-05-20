d4b846e9b1ed74d0c8b38fe51c21ba404e7e7963
jgdml <jgdml2801@gmail.com>
2022-05-20 18:21:26 -0300

    Customer constructor

--- Customer.h
+++ Customer.h
@@ -2,16 +2,18 @@
 #define _market_customer_h_
 
 #include "Util.h"
-
+#include <string.h>
 class Customer {
 private:
     int id;
     char name[50];
     double totalSpent;
 public:
-    explicit Customer() = default;
-
-    char* getCustomerName();
+    explicit Customer(int _id, const char* _name, double _totalSpent){
+        id = _id;
+        strcpy(name, _name);
+        totalSpent = _totalSpent;
+    };
 
 };
 
