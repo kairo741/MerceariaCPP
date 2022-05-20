f8439e832a650ca12cc2419fef570c3cf096d4dc
RafaelBicario <rafabicario@outlook.com>
2022-05-20 20:57:36 -0300

    Criação do CustomerRepository.cpp

---
+++ CustomerRepository.cpp
@@ -0,0 +1,68 @@
+#include "CustomerRepository.h"
+
+void CustomerRepository::readCustomers(){
+    FILE *filePointer;
+
+    filePointer = fopen(customerFile, "rb");
+    Customer buffer;
+
+    fseek(filePointer, 0, SEEK_SET);
+
+    while (true){
+        fread(&buffer, sizeof(buffer), 1, filePointer);
+        if (feof(filePointer)){
+            break;
+        }
+
+        Util::getUpLines();
+        printf("   Id: %d\n", buffer.getId());
+        printf("   Nome: %s\n", buffer.getName());
+        printf("   Total Gasto: %f.2", buffer.getTotalSpent());
+        Util::getUnderLines();
+    }
+
+    fclose(filePointer);
+}
+
+
+void CustomerRepository::writeCustomers(Customer c){
+    FILE *filePointer;
+    filePointer = fopen(customerFile, "ab");
+
+    fwrite(&c, 1, sizeof(c), filePointer);
+    fclose(filePointer);
+}
+
+
+void CustomerRepository::replaceOrDeleteCustomer(Customer c, bool removeData){
+    FILE *filePointer;
+    filePointer = fopen(customerFile, "rb");
+    Customer buffer;
+    fseek(filePointer, 0, SEEK_SET);
+
+    FILE *newFilePointer;
+    newFilePointer = fopen(customerTempFile, "ab");
+    Customer newBuffer;
+
+    while (true){
+        fread(&buffer, sizeof(buffer), 1, filePointer);
+
+        if (feof(filePointer)){
+            break;
+        }
+
+        if (buffer.getId() == c.getId()){
+            if (!removeData){
+                fwrite(&c, 1, sizeof(c), newFilePointer);
+            }
+        }
+        else{
+            fwrite(&buffer, 1, sizeof(buffer), newFilePointer);
+        }
+    }
+
+    fclose(filePointer);
+    fclose(newFilePointer);
+    remove(customerFile);
+    rename(customerTempFile, customerFile);
+}
